edb600636e4344ea20cca4c4a50890f552277a90
therajat14 <heylegend14@gmail.com>
2025-11-05 22:51:04 +0530

    [200~feat: add draggable motion box with responsive drag constraints and hover effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { motion } from "motion/react";
-
+import { motion } from "framer-motion";
+import { useRef } from "react";
 const App = () => {
+  const boxRef = useRef(null);
+  console.log(boxRef);
   return (
-    <div className="content-center">
-      <motion.button
-        animate={{
-          x: 300,
-          y: 300,
-          transition: { duration: 2 },
+    <div
+      ref={boxRef}
+      className="flex h-screen items-center justify-center bg-gray-900 font-mono"
+    >
+      <motion.div
+        initial={{ opacity: 0, x: -200, scale: 0.5 }}
+        animate={{ opacity: 1, x: 0, scale: 1, rotate: 360 }}
+        transition={{
+          duration: 2,
+          ease: "easeOut",
         }}
+        whileHover={{ scale: 1.2, rotate: 10 }}
+        whileTap={{ scale: 0.9, rotate: -10 }}
+        drag
+        dragConstraints={boxRef}
+        className="flex h-40 w-40 items-center justify-center rounded-xl bg-blue-900 text-gray-300"
       >
-        Submit
-      </motion.button>
-      <h1>Hi there</h1>
+        Hover Me
+      </motion.div>
     </div>
   );
 };
